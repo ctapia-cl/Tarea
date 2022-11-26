ce5fc20530af746a243788009a9a7bfe4c7a4e44
hernanAcevedoM <118073250+hernanAcevedoM@users.noreply.github.com>
2022-11-26 13:32:07 -0300

    Version 1.4

--- app/src/main/java/com/example/tarea/Tareas.java
+++ app/src/main/java/com/example/tarea/Tareas.java
@@ -4,5 +4,29 @@ import java.io.Serializable;
 
 //array list de tareas
 public class Tareas implements Serializable {
+    private String titulo;
+    private String descripcion;
+
+    public void setTitulo(String nuevoTitulo){
+
+        titulo = nuevoTitulo;
+    }
+
+    public String getTitulo(){
+
+        return titulo;
+    }
+
+
+
+    public void setDescripcion(String nuevoDescripcion){
+
+        descripcion = nuevoDescripcion;
+    }
+
+    public String getDescripcion(){
+
+        return descripcion;
+    }
 
 }
