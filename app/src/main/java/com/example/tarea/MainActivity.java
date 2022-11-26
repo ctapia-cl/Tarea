7b80f2506c993cbcc3bf893ff9138144525a5f86
Cristian Tapia <tapiarodriguez@gmail.com>
2022-11-26 15:40:30 -0300

    Version 1.6

--- app/src/main/java/com/example/tarea/MainActivity.java
+++ app/src/main/java/com/example/tarea/MainActivity.java
@@ -2,18 +2,16 @@ package com.example.tarea;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;   
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
     private TextInputLayout tilTitulo, tilDescripcion;
-    private Button btnIngresar, btnModificar, btnBuscar;
+    private Button btnIngresar, btnModificar;
+    private ImageButton btnBuscar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,49 +22,20 @@ public class MainActivity extends AppCompatActivity {
         eventos();
     }
 
-    private void mostrarNombreEnOtraActividad(){
-        Comuna comunaUno = new Comuna();
-
-        comunaUno.setNombre("Quinta Normal");
-        comunaUno.setPoblacion(10000);
-        comunaUno.setAlcalde("Juanito Perez");
-        comunaUno.setGrupoGSE("C2");
-        comunaUno.setMt2(20000.76f);
-        comunaUno.setDireccionMunicipalidad("Los pinos oriente 1234");
-
-
-        String nombre = tilTitulo.getEditText().getText().toString();
-
-        Intent segundaPantalla = new Intent(this,SegundaActividad.class);
-        segundaPantalla.putExtra("datoNombre", nombre);
-        segundaPantalla.putExtra("datosComuna", comunaUno);
-
-        startActivity(segundaPantalla);
-    }
 
 
 
     //region Eventos y referencias
     private void eventos(){
-        btnIngresar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mostrarNombreEnOtraActividad();
-            }
-        });
-        btnModificar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+
     }
 
     private void referencias(){
         tilTitulo = findViewById(R.id.tilTitulo);
-        tilDescripcion = findViewById(R.id.tilDescripcion);
+        tilDescripcion = findViewById(R.id.tilTitulo);
         btnIngresar = findViewById(R.id.btnIngresar);
         btnModificar = findViewById(R.id.btnModificar);
         btnBuscar = findViewById(R.id.btnBuscar);
     }
-//endregion
+    //endregion
+}
