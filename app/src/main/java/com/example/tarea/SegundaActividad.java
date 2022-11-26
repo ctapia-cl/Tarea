daf4a8bd8e3ad82a4462f4f94871cdaafe217b4b
hernanAcevedoM <118073250+hernanAcevedoM@users.noreply.github.com>
2022-11-26 13:24:35 -0300

    Version 1.3

--- app/src/main/java/com/example/tarea/SegundaActividad.java
+++ app/src/main/java/com/example/tarea/SegundaActividad.java
@@ -1,15 +1,38 @@
 package com.example.tarea;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
+
+
 public class SegundaActividad extends AppCompatActivity {
+    //referencias
+    private Button btnFiltrar;
+    private ListView ltvTareas;
+    private TextInputLayout tilBuscar;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda_actividad);
+
+        referencias();
+
     }
+
+    //region referencias y eventos
+    private void referencias() {
+        btnFiltrar = findViewById(R.id.btnFiltrar);
+        ltvTareas = findViewById(R.id.ltvTareas);
+        tilBuscar = findViewById(R.id.tilBuscar);
+
+    }
+
+    //endregion
 }
---
+++ app/src/main/java/com/example/tarea/Tareas.java
@@ -0,0 +1,8 @@
+package com.example.tarea;
+
+import java.io.Serializable;
+
+//array list de tareas
+public class Tareas implements Serializable {
+
+}
