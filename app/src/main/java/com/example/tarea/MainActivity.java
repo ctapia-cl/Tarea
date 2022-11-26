1d02544d8c49d3f00e22b6d21be021a281769461
hernanAcevedoM <118073250+hernanAcevedoM@users.noreply.github.com>
2022-11-26 12:05:59 -0300

    Version 1.2

--- app/src/main/java/com/example/tarea/MainActivity.java
+++ app/src/main/java/com/example/tarea/MainActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Button;   
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
